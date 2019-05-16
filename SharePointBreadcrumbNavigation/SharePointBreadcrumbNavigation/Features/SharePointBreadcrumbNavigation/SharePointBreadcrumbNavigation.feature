77120f8b0961a254d8cae3495356f61aba019f76
Yunus Emre Araç <earac@nuevo.com.tr>
2019-05-16 12:09:03 +0300

    Project Commit

---
+++ SharePointBreadcrumbNavigation/SharePointBreadcrumbNavigation/Features/SharePointBreadcrumbNavigation/SharePointBreadcrumbNavigation.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="d5d9a226-4743-45d0-af02-85dd756b52c1" featureId="d5d9a226-4743-45d0-af02-85dd756b52c1" imageUrl="" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="SharePointBreadcrumbNavigation" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="f974f69d-212f-4c3c-aeea-1a0731d4063e" />
+  </projectItems>
+</feature>
---
+++ SharePointBreadcrumbNavigation/SharePointBreadcrumbNavigation/SharePointBreadcrumbNavigation/SharePointBreadcrumbNavigationUserControl.ascx.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.SharePoint.Publishing;
+using Microsoft.SharePoint.Publishing.Navigation;
+using Microsoft.SharePoint.WebControls;
+using System;
+using System.Web;
+using System.Web.UI;
+
+namespace SharePointBreadcrumbNavigation.SharePointBreadcrumbNavigation
+{
+    public partial class SharePointBreadcrumbNavigationUserControl : UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Page is UnsecuredLayoutsPageBase)
+            {
+                ContentMap.SiteMapProvider = "SPXmlContentMapProvider";
+            }
+            else if (Page is PublishingLayoutPage)
+            {
+                ContentMap.RenderCurrentNodeAsLink = true;
+
+                PortalSiteMapProvider provider = SiteMap.Providers["CurrentNavSiteMapProviderNoEncode"] as PortalSiteMapProvider;
+
+                if (provider != null)
+                {
+                    provider.IncludePages = PortalSiteMapProvider.IncludeOption.Always;
+                    ContentMap.Provider = provider;
+                }
+                else
+                {
+                    ContentMap.SiteMapProvider = "CurrentNavSiteMapProviderNoEncode";
+                }
+            }
+            else
+            {
+                ContentMap.SiteMapProvider = "SPContentMapProvider";
+            }
+        }
+    }
+}
